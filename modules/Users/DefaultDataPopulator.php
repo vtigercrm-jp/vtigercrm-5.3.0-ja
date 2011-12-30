3f2ce3adc8b0fe46a0693ec794de14e191006e52
vtigercrm-jp <admin@vtigercrm.jp>
2011-12-31 00:50:32 +0900

    set Japanese language info into vtiger_language table and set it as default, during default data populator operation. Default user language becomes ja

--- modules/Users/DefaultDataPopulator.php
+++ modules/Users/DefaultDataPopulator.php
@@ -2011,8 +2011,11 @@ $body='<table width="700" cellspacing="0" cellpadding="0" border="0" align="cent
 	//Register default language English
 	require_once('vtlib/Vtiger/Language.php');
 	$vtlanguage = new Vtiger_Language();
-	$vtlanguage->register('en_us','US English','English',true,true,true);
-	
+// JFV - set Japanese language info into vtiger_language table and set it as default, during default data populator operation. Default user language becomes ja
+//	$vtlanguage->register('en_us','US English','English',true,true,true);
+	$vtlanguage->register('en_us','US English','English',false,true,true);
+	$vtlanguage->register('ja','Japanese ( “ú–{Œê )','Japanese',true,true,true);
+// JFV END
 	$this->initWebservices();
 
 	/**
